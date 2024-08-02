3e56acf1aaae150da996f835d1355ec57882139f
zehan12 <zehan9211@gmail.com>
2024-08-03 01:06:39 +0530

    add theme toggle

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,12 @@
+import { ThemeToggle } from "../common";
+
 export const Header = () => {
     return (
-        <header>
-            <div className="sticky top-0 z-50 flex items-center justify-between p-5 bg-white shadow-sm dark:shadow-transparent dark:border-b-2 dark:border-b-gray-800 dark:bg-dark-800">
-                <div className="flex items-center gap-2">
-                    <strong>Pantry Tracker App</strong>
-                </div>
+        <header className="sticky top-0 z-50 p-4 flex items-center justify-between p-s bg-background text-foreground shadow-sm dark:shadow-transparent dark:border-b-2 dark:border-b-gray-800 dark:bg-dark-800">
+            <div className="flex items-center gap-2">
+                <strong>Pantry Tracker App</strong>
             </div>
+            <ThemeToggle />
         </header>
     );
 };
