049784831b13d628651ecfeb2bc2dd85bd8aeba3
zehan12 <zehan9211@gmail.com>
2024-08-03 00:26:33 +0530

    add header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+        <header>
+            <div className="sticky top-0 z-50 flex items-center justify-between p-5 bg-white shadow-sm dark:shadow-transparent dark:border-b-2 dark:border-b-gray-800 dark:bg-dark-800">
+                <div className="flex items-center gap-2">
+                    <strong>Pantry Tracker App</strong>
+                </div>
+            </div>
+        </header>
+    );
+};
